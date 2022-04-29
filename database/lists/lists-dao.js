e24539312c3d383b39dffb0d8ff0f57e658196c3
macksonyqli <li.yuanqia@northeastern.edu>
2022-04-28 20:57:03 -0500

    update

--- database/lists/lists-dao.js
+++ database/lists/lists-dao.js
@@ -1,16 +1,22 @@
 import listsModel from "./lists-model.js";
 
 const userSaveBook = async (userId, bookID) => {
-    const bookInList = await listsModel.create({bookID: bookID, listedBy: userId})
+    const bookInList = await listsModel.create({listedBy: userId, bookID: bookID})
     return bookInList
 }
 
 const userUnsaveBook = async (userId, bookID) => {
-    listsModel.deleteOne({bookID: bookID, listedBy: userId})
+    return listsModel.deleteOne({bookID: bookID, listedBy: userId})
 }
-const findAllBooksSavedByUser = async (userId) =>{
-    listsModel.find({listedBy: userId})
+
+const findAllBooksSavedByUser = (userId) => {
+    return listsModel.find({listedBy: userId})
+}
+
+const findUserSavedBook = async (userId, bookID) => {
+    return listsModel.findOne({bookID: bookID, listedBy: userId})
 }
 export default {
-    userSaveBook, userUnsaveBook, findAllBooksSavedByUser
+    userSaveBook, userUnsaveBook, findAllBooksSavedByUser,
+    findUserSavedBook
 }
