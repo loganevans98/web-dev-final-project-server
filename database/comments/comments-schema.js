56d1c0d0b724f00244287d829196e0df399299bf
loganevans98 <loganevans98@gmail.com>
2022-04-28 22:16:12 -0400

    reworking

--- database/comments/comments-schema.js
+++ database/comments/comments-schema.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 const commentsSchema = mongoose.Schema({
     comment: String,
     bookID: String,
+    bookTitle: String,
     commenterEmail: String,
+    bookLink: String,
     commenter: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'UsersModel'}
